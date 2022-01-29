f9fb3aef458169d5e9880aeb6f4f87f9a0431013
CamIsHereRN <cameron_bolyard@yahoo.com>
2022-01-29 10:39:33 -0600

    added Swerve Code

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,35 +6,67 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
-import frc.robot.commands.ExampleCommand;
-import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import frc.robot.subsystems.*;
+import frc.robot.commands.*;
 
 /**
- * This class is where the bulk of the robot should be declared. Since Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
- * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * This class is where the bulk of the robot should be declared. Since
+ * Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in
+ * the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of
+ * the robot (including
  * subsystems, commands, and button mappings) should be declared here.
  */
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
-  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
+  private XboxController joystick;
 
-  private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
+  private final SwerveDrive swerveDrive;
+  private final WheelDrive fL;
+  private final WheelDrive fR;
+  private final WheelDrive bL;
+  private final WheelDrive bR;
+  public final DriveJoystick driveJoystick;
+  private final SubsystemBase[] subsystemList;
 
-  /** The container for the robot. Contains subsystems, OI devices, and commands. */
-  public RobotContainer() {
+  /**
+   * The container for the robot. Contains subsystems, OI devices, and commands.
+   */
+  public RobotContainer(XboxController controller) {
     // Configure the button bindings
     configureButtonBindings();
+    joystick = controller;
+
+    fL = new WheelDrive("FL", 5, 1, 15, 0.00, 2000, 1023, true);
+    fR = new WheelDrive("FR", 6, 2, 15, 0.00, 20, 1023, true);
+    bL = new WheelDrive("BL", 7, 3, 15, 0.00, 2000, 1023, true);
+    bR = new WheelDrive("BR", 8, 4, 15, 0.00, 20, 1023, true);
+    swerveDrive = new SwerveDrive(bR, bL, fR, fL, 27.0, 21.0);
+    subsystemList = new SubsystemBase[5];
+    subsystemList[0] = swerveDrive;
+    subsystemList[1] = fL;
+    subsystemList[2] = fR;
+    subsystemList[3] = bL;
+    subsystemList[4] = bR;
+
+    driveJoystick = new DriveJoystick(subsystemList[0], joystick, swerveDrive);
+
   }
 
   /**
-   * Use this method to define your button->command mappings. Buttons can be created by
+   * Use this method to define your button->command mappings. Buttons can be
+   * created by
    * instantiating a {@link GenericHID} or one of its subclasses ({@link
-   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
+   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing
+   * it to a {@link
    * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
    */
-  private void configureButtonBindings() {}
+  private void configureButtonBindings() {
+  }
 
   /**
    * Use this to pass the autonomous command to the main {@link Robot} class.
@@ -43,6 +75,6 @@ public class RobotContainer {
    */
   public Command getAutonomousCommand() {
     // An ExampleCommand will run in autonomous
-    return m_autoCommand;
+    return null;
   }
 }
