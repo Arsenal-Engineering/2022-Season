2fb90e6cbb760a0122f2559a298ef8f82f700ac8
grancai99 <caitlynrosegran@gmail.com>
2022-03-23 22:17:24 -0500

    finally delete ex auto command

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,15 +7,12 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj.Timer;
 
 public class Robot extends TimedRobot {
   private static RobotContainer robotContainer;
   private Timer timer;
-
-  private Command m_autonomousCommand;
 
   @Override
   public void robotInit() {
@@ -40,13 +37,6 @@ public class Robot extends TimedRobot {
 
   @Override
   public void autonomousInit() {
-    // m_autonomousCommand = robotContainer.getAutonomousCommand();
-
-    // schedule the autonomous command (example)
-    // if (m_autonomousCommand != null) {
-    //   m_autonomousCommand.schedule();
-    // }
-
     robotContainer.getSwerveDrive().setBrakeMode(true);
     timer.reset();
     timer.start();
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,7 +7,6 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
@@ -184,10 +183,5 @@ public class RobotContainer {
 
   public DriveAuto createDriveAuto(double x1, double y1, double x2) {
     return new DriveAuto(x1, y1, x2, swerveDrive);
-  }
-
-  public Command getAutonomousCommand() {
-    // An ExampleCommand will run in autonomous
-    return null;
   }
 }
